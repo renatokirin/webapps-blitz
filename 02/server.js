9012c643289b4725656a4bd5165dd8c126b63d28
Renato Kirin <renatokirin@gmail.com>
2022-11-22 08:34:53 +0100

    saveItem

--- 02/server.js
+++ 02/server.js
@@ -11,7 +11,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const database = client.db("test");
 const collection = database.collection("blitz");
 
+app.post("/saveItem", async (req, response) => {
 
+    let item = req.body;
+
+
+    collection.insertOne(item).then(result => {
+        return response.status(200).json({ status: "OK" ,"message": `Item ${item.name} saved in DB` });
+    }).catch(err => {
+        return response.status(500).json({ "message": err });
+    });
+});
 
 
 app.listen(port, () => console.log(`Works on port ${port}`));
